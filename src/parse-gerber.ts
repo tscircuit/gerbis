be54a5bdef5e097862544a26b1c661cf68087297
Severin Ibarluzea <seveibar@gmail.com>
2022-08-28 18:12:09 -0700

    packaging

---
+++ src/parse-gerber.ts
@@ -0,0 +1,10 @@
+import nearley from "nearley"
+import grammar from "./nearley/gerber"
+
+export const parseGerber = (input: string) => {
+  const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar))
+  parser.feed(input.replace(/\n/g, ""))
+  return parser.results
+}
+
+export default parseGerber
